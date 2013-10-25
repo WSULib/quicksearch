b983cab7c9547d3f9aa6db293b8a8e4a90ed07a5
Graham Hukill <ghukill@gmail.com>
2013-10-25 12:14:28 -0400

    Bento matches A-Z search
    Still room for improvements though; both A-Z and bento should break down multiple words into OR searches.

--- php/databases.php
+++ php/databases.php
@@ -1,6 +1,7 @@
 <?php
 //script to return results from A-Z database for Bento Box interface
 $search_string = $_GET['search_string'];
+$search_string = str_replace(" ","+",$search_string);
 $json = file_get_contents("http://www.lib.wayne.edu/inc/dbs/resource_database_v2.php?q=$search_string");
 echo $json;
 ?>
