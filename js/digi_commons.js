afbbfb42bdfb58f1db5ad9b7fb355d118a4b896a
Graham Hukill <ghukill@gmail.com>
2014-06-04 13:44:39 -0400

    Fixed for 2+ search terms

--- js/digi_commons.js
+++ js/digi_commons.js
@@ -1,7 +1,7 @@
 function digi_commons_query(search_string){
 
     // encode the string
-    encoded_search_string = search_string.replace(" ","+");
+    encoded_search_string = search_string.replace(/\s/g,'+');
 
     // create POST data object
     dataObject = {};
