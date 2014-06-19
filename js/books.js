aa975f83836e099d864c4afe16bf9199e6385641
Cole Hudson <cole.hudson@wayne.edu>
2014-06-19 09:11:06 -0400

    added see on shelf and call number/availablity to books bento box

--- js/books.js
+++ js/books.js
@@ -4,7 +4,7 @@ var books_sorted = null;
 var count = null;
 var z = null;
 
-function books_query(search_string){  
+function books_query(search_string){
   search_string = search_string.replace(/:|\]|\[/g,'');
   var json_books_response = null;
 
@@ -30,8 +30,6 @@ function Success (response){
     $("#books .box_results").empty();
     // hides loading animation
 	  $("#books .box_loading_animation").hide();
-
-//make a global variable with the books' ajax response
     books_object = response;
 
     //function to test if object is empty
@@ -41,94 +39,80 @@ function Success (response){
     }
 
 
-if (books_object.PAGEINFO.ENTRYCOUNT !== '0') {
+if (books_object.bookTotal !== 0) {
+  count = books_object.bookTotalComplete;
 
-
-if (books_object.PAGEINFO.ENTRYCOUNT === '1') {
-  count = books_object.Heading.HeadingSize;
-  if ( isEmpty(books_object.Heading.Title.TitleField) === true) {
-    title = "eResource - Click For Title";
-  }
-  else {         
-  title = books_object.Heading.Title.TitleField.VARFLDPRIMARYALTERNATEPAIR.VARFLDPRIMARY.VARFLD.DisplayForm;
-  }
-  var urlPrefix = "http://elibrary.wayne.edu/search~S47?/X"+search_string+"&searchscope=47&SORT=D/X"+search_string+"&searchscope=47&SORT=D&SUBKEY="+search_string+"/1%2C"+count+"%2C"+count+"%2CB/frameset&FF=X"+search_string+"&searchscope=47&SORT=D&SUBKEY="+search_string+"&"
-  if (typeof books_object.Heading.Title.RecordId.RecordKey === "undefined") {
-  var url = "http://elibrary.wayne.edu/search~/?searchtype=X&searcharg="+search_string;         
+if (books_object.bookTotal === 1) {
+  var title = books_object.title[0];
+  var urlPrefix = "http://elibrary.wayne.edu/record=";
+ 
+  if (!books_object.bibNum[0]) {
+    var url = "http://elibrary.wayne.edu/search~/?searchtype=X&searcharg="+search_string;         
+    $("#books .box_results").append("<div class='indiv-result'><p class='title'><a href='"+url+"'>"+title+"</a></p></div>");
   }
   else {
-  var recNum = books_object.Heading.Title.RecordId.RecordKey;
-  var recAdd = i+1;
-  var rec =  recAdd+"%2C"+recAdd+"%2C";
-  var url = urlPrefix+rec;
- // var url = urlPrefix+recNum;
+          var recNum = books_object.bibNum[0];
+          var url = urlPrefix+recNum;
+          var hasMultipleHoldings = $.isArray(books_object.holdings_info[0].holding) ? true : false;
+          var TypeofHoldingStatus = (hasMultipleHoldings) ? books_object.holdings_info[0].holding[0].publicNote : books_object.holdings_info[0].holding.publicNote;
+          var holdingStatus = (isEmpty(TypeofHoldingStatus)) ? "" : TypeofHoldingStatus;
+          var stackviewLink = (books_object.MatType[0] == "PRINT" && books_object.lc[i] !== null) ? "<a href='http://library.wayne.edu/test/cole/stackview-master/?q="+books_object.lc[0]+"' target='_blank'> <b>See on Shelf</b> <i class='glyphicon glyphicon-share-alt'></i></a><br/>": " <br/>";
+          var lc = (books_object.lc[i] == null) ? "" : "<br/>"+books_object.lc[i]+"";
+          $("#books .box_results").append("<div class='indiv-result'><p class='title'><a href='"+url+"'>"+title+"</a>"+stackviewLink+holdingStatus+lc+"</p></div>");
   }
-  $("#books .box_results").append("<div class='indiv-result'><p class='title'><a href='"+url+"'>"+title+"</a></p></div>");
   $("#books .box_results").append("<a href='http://elibrary.wayne.edu/search~/?searchtype=X&searcharg="+search_string+"' onclick=\"javascript:_paq.push(['trackPageView', 'View More']);\"><em>View more results...("+count+")</em></a></span>");  
 
 }
 
-else if (books_object.PAGEINFO.ENTRYCOUNT === '2'){
+else if (books_object.bookTotal === 2){
 
 for (var i = 0; i < 2; i++) {
-         //make some shortened variables for the data you want to mess with
-              
-              count = books_object.Heading.HeadingSize;
-              if ( isEmpty(books_object.Heading.Title[i].TitleField) === true) {
-                  title = "eResource - Click For Title";
-                }
-               else {         
-                title = books_object.Heading.Title[i].TitleField.VARFLDPRIMARYALTERNATEPAIR.VARFLDPRIMARY.VARFLD.DisplayForm;
-                }
-                var urlPrefix = "http://elibrary.wayne.edu/search~S47?/X"+search_string+"&searchscope=47&SORT=D/X"+search_string+"&searchscope=47&SORT=D&SUBKEY="+search_string+"/1%2C"+count+"%2C"+count+"%2CB/frameset&FF=X"+search_string+"&searchscope=47&SORT=D&SUBKEY="+search_string+"&"
-              if (typeof books_object.Heading.Title[i].RecordId.RecordKey === "undefined") {
-                var url = "http://elibrary.wayne.edu/search~/?searchtype=X&searcharg="+search_string;         
-                }
-                else {
-               var recNum = books_object.Heading.Title[i].RecordId.RecordKey;
-               var recAdd = i+1;
-               var rec =  recAdd+"%2C"+recAdd+"%2C";
-               var url = urlPrefix+rec;
-             //  var url = urlPrefix+recNum;
-              }
+      var title = books_object.title[i];
+      var urlPrefix = "http://elibrary.wayne.edu/record=";
+
+      if (!books_object.bibNum[i]) {
+        var url = "http://elibrary.wayne.edu/search~/?searchtype=X&searcharg="+search_string;
         $("#books .box_results").append("<div class='indiv-result'><p class='title'><a href='"+url+"'>"+title+"</a></p></div>");
-}
+        }
+      else {
+          var recNum = books_object.bibNum[i];
+          var url = urlPrefix+recNum;
+          var hasMultipleHoldings = $.isArray(books_object.holdings_info[i].holding) ? true : false;
+          var TypeofHoldingStatus = (hasMultipleHoldings) ? books_object.holdings_info[i].holding[0].publicNote : books_object.holdings_info[i].holding.publicNote;
+          var holdingStatus = (isEmpty(TypeofHoldingStatus)) ? "" : TypeofHoldingStatus;
+          var stackviewLink = (books_object.MatType[i] == "PRINT" && books_object.lc[i] !== null) ? "<a href='http://library.wayne.edu/test/cole/stackview-master/?q="+books_object.lc[i]+"' target='_blank'> <b>See on Shelf</b> <i class='glyphicon glyphicon-share-alt'></i></a><br/>": " <br/>";
+          var lc = (books_object.lc[i] == null) ? "" : "<br/>"+books_object.lc[i]+"";
+          $("#books .box_results").append("<div class='indiv-result'><p class='title'><a href='"+url+"'>"+title+"</a>"+stackviewLink+holdingStatus+lc+"</p></div>");
+        }
+      }
         $("#books .box_results").append("<a href='http://elibrary.wayne.edu/search~/?searchtype=X&searcharg="+search_string+"' onclick=\"javascript:_paq.push(['trackPageView', 'View More']);\"><em>View more results...("+count+")</em></a></span>");  
 
 }
 else {
 for (var i = 0; i < 3; i++) {
-         //make some shortened variables for the data you want to mess with
-              
-              count = books_object.Heading.HeadingSize;
-              if ( isEmpty(books_object.Heading.Title[i].TitleField) === true) {
-                  title = "eResource - Click For Title";
-                }
-               else {         
-                title = books_object.Heading.Title[i].TitleField.VARFLDPRIMARYALTERNATEPAIR.VARFLDPRIMARY.VARFLD.DisplayForm;
+      var title = books_object.title[i];
+      var urlPrefix = "http://elibrary.wayne.edu/record=";
 
-                }
-                //http://elibrary.wayne.edu/search~S47?/Xgreen+technology&searchscope=47&SORT=D/Xgreen+technology&searchscope=47&SORT=D&SUBKEY=green+technology/1%2C513%2C513%2CB/frameset&FF=Xgreen+technology&searchscope=47&SORT=D&SUBKEY=green+technology&1%2C1%2C
-                var urlPrefix = "http://elibrary.wayne.edu/search~S47?/X"+search_string+"&searchscope=47&SORT=D/X"+search_string+"&searchscope=47&SORT=D&SUBKEY="+search_string+"/1%2C"+count+"%2C"+count+"%2CB/frameset&FF=X"+search_string+"&searchscope=47&SORT=D&SUBKEY="+search_string+"&"
-                //var urlPrefix = "http://elibrary.wayne.edu/record=";
-              if (typeof books_object.Heading.Title[i].RecordId.RecordKey === "undefined") {
-                var url = "http://elibrary.wayne.edu/search~/?searchtype=X&searcharg="+search_string;         
-                }
-                else {
-               var recNum = books_object.Heading.Title[i].RecordId.RecordKey;
-               //1%2C1%2C
-               var recAdd = i+1;
-               var rec =  recAdd+"%2C"+recAdd+"%2C";
-               var url = urlPrefix+rec;
-              }
+      if (!books_object.bibNum[i]) {
+        var url = "http://elibrary.wayne.edu/search~/?searchtype=X&searcharg="+search_string;         
         $("#books .box_results").append("<div class='indiv-result'><p class='title'><a href='"+url+"'>"+title+"</a></p></div>");
         }
-      
+        else {
+          var recNum = books_object.bibNum[i];
+          var url = urlPrefix+recNum;
+          var hasMultipleHoldings = $.isArray(books_object.holdings_info[i].holding) ? true : false;
+          var TypeofHoldingStatus = (hasMultipleHoldings) ? books_object.holdings_info[i].holding[0].publicNote : books_object.holdings_info[i].holding.publicNote;
+          var holdingStatus = (isEmpty(TypeofHoldingStatus)) ? "" : TypeofHoldingStatus;
+          var stackviewLink = (books_object.MatType[i] == "PRINT" && books_object.lc[i] !== null) ? "<a href='http://library.wayne.edu/test/cole/stackview-master/?q="+books_object.lc[i]+"' target='_blank'> <b>See on Shelf</b> <i class='glyphicon glyphicon-share-alt'></i></a><br/>": " <br/>";
+          var lc = (books_object.lc[i] == null) ? "" : "<br/>"+books_object.lc[i]+"";
+          $("#books .box_results").append("<div class='indiv-result'><p class='title'><a href='"+url+"'>"+title+"</a>"+stackviewLink+holdingStatus+lc+"</p></div>");
+         }
+      }
         $("#books .box_results").append("<a href='http://elibrary.wayne.edu/search~/?searchtype=X&searcharg="+search_string+"' onclick=\"javascript:_paq.push(['trackPageView', 'View More']);\"><em>View more results...("+count+")</em></a></span>");  
     }
 }
 //If no results, then display no results found
-      if (books_object.PAGEINFO.ENTRYCOUNT === '0') {  
+      if (books_object.bookTotal === 0) {  
           $("#books .box_results").append("<span class=\"no-res\">No material results were found.  Please try another search in the <a href='http://elibrary.wayne.edu'>Catalog</a></span>");    
       }
 }
@@ -136,6 +120,11 @@ for (var i = 0; i < 3; i++) {
 function Error (response){
     console.log("Error");
     console.log(response);
+    //clear previous results
+    $("#books .box_results").empty();
+    // hides loading animation
+    $("#books .box_loading_animation").hide();
+    $("#books .box_results").append("<span class=\"no-res\">No material results were found.  Please try another search in the <a href='http://elibrary.wayne.edu'>Catalog</a></span>");
     }
 
         });
